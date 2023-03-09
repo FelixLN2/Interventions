4fe6cdc1e34713cce8077e427ea29cc47b788fcf
Felix Laprise-Narbonne <2215307@cmontmorency.qc.ca>
2023-03-09 16:46:09 -0500

    tp6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemeComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
