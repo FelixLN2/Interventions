47c3d242620d6fe8b145bb56fca71bfd8bc39878
Felix Laprise-Narbonne <2215307@cmontmorency.qc.ca>
2023-05-04 14:22:45 -0400

    tp20

---
+++ src/app/probleme/probleme.ts
@@ -0,0 +1,12 @@
+export interface IProbleme {
+    id: number,
+    prenom: string,
+    nom: string,
+    courriel?: string,
+    //courrielConfirmation?: string,
+    telephone?: string,
+    notification: string,
+    noUnite?: string,
+    descriptionProbleme:string,
+    //dateProbleme?: Date
+}
